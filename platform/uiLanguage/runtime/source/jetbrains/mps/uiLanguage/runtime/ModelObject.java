381886216b15a42d38ce4abcceffbeaa23a08c6f
Konstantin Solomatov <kostik@jetbrains.com>
2008-03-06 18:33:24 +0300

    model editor

---
+++ platform/uiLanguage/runtime/source/jetbrains/mps/uiLanguage/runtime/ModelObject.java
@@ -0,0 +1,29 @@
+package jetbrains.mps.uiLanguage.runtime;
+
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+
+public class ModelObject {
+  private PropertyChangeSupport myPropertyChangeSupport = new PropertyChangeSupport(this);
+
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    myPropertyChangeSupport.addPropertyChangeListener(listener);
+  }
+
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    myPropertyChangeSupport.removePropertyChangeListener(listener);
+  }
+
+  public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    myPropertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+  }
+
+  public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    myPropertyChangeSupport.removePropertyChangeListener(propertyName, listener);
+  }
+
+  protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+    myPropertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+  }
+}
+
