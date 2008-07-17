cab01556ea99082f3d54e6397c7f8982752052a4
Konstantin Solomatov <kostik@jetbrains.com>
2008-07-17 14:01:05 +0400

    move cell actions to a separate package

--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/ComponentInstance_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/ComponentInstance_Editor.java
@@ -25,7 +25,7 @@ import jetbrains.mps.nodeEditor.AbstractCellProvider;
 import jetbrains.mps.bootstrap.editorLanguage.cellProviders.RefNodeListHandler;
 import jetbrains.mps.smodel.action.NodeFactoryManager;
 import jetbrains.mps.nodeEditor.EditorCellAction;
-import jetbrains.mps.nodeEditor.CellAction_DeleteNode;
+import jetbrains.mps.nodeEditor.cellActions.CellAction_DeleteNode;
 import jetbrains.mps.nodeEditor.DefaultReferenceSubstituteInfo;
 import jetbrains.mps.nodeEditor.DefaultChildSubstituteInfo;
 
--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/StandardDialog_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/StandardDialog_Editor.java
@@ -21,7 +21,7 @@ import jetbrains.mps.nodeEditor.EditorManager;
 import jetbrains.mps.bootstrap.editorLanguage.cellProviders.RefNodeListHandler;
 import jetbrains.mps.smodel.action.NodeFactoryManager;
 import jetbrains.mps.nodeEditor.EditorCellAction;
-import jetbrains.mps.nodeEditor.CellAction_DeleteNode;
+import jetbrains.mps.nodeEditor.cellActions.CellAction_DeleteNode;
 import jetbrains.mps.nodeEditor.DefaultReferenceSubstituteInfo;
 import jetbrains.mps.nodeEditor.DefaultChildSubstituteInfo;
 
