f9e9b4ee4ccd53300539f1f432c60f6cd1e4f65d
Darja Chembrovskaya <Darja.Chembrovskaya@jetbrains.com>
2008-11-14 20:38:26 +0300

    rename temporary style attribute (".._with_measure")

--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeDeclaration_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeDeclaration_Editor.java
@@ -249,7 +249,7 @@ public class AttributeDeclaration_Editor extends DefaultNodeEditor {
     {
       Style inlineStyle = new Style(editorCell) {
         {
-          this.set(StyleAttributes.PADDING_LEFT_WITH_MEASURE, new Padding(1.0, "spaces"));
+          this.set(StyleAttributes.PADDING_LEFT, new Padding(1.0, "spaces"));
         }
 
       };
--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeReferenceOperation_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeReferenceOperation_Editor.java
@@ -111,7 +111,7 @@ public class AttributeReferenceOperation_Editor extends DefaultNodeEditor {
       {
         Style inlineStyle = new Style(editorCell) {
           {
-            this.set(StyleAttributes.PADDING_RIGHT_WITH_MEASURE, new Padding(0.0, "spaces"));
+            this.set(StyleAttributes.PADDING_RIGHT, new Padding(0.0, "spaces"));
           }
 
         };
