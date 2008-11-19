4c2a993088abba465df2312ce8626dcabf77b23b
Darja Chembrovskaya <Darja.Chembrovskaya@jetbrains.com>
2008-11-19 18:45:57 +0300

    rename EnumMeasure

--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeDeclaration_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/AttributeDeclaration_Editor.java
@@ -17,7 +17,7 @@ import jetbrains.mps.lang.editor.cellProviders.RefNodeCellProvider;
 import jetbrains.mps.nodeEditor.style.Style;
 import jetbrains.mps.nodeEditor.style.StyleAttributes;
 import jetbrains.mps.nodeEditor.style.Padding;
-import jetbrains.mps.nodeEditor.style.EnumMeasure;
+import jetbrains.mps.nodeEditor.style.Measure;
 import jetbrains.mps.baseLanguage.editor.BaseLanguageStyle_StyleSheet;
 import jetbrains.mps.smodel.IScope;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
@@ -250,7 +250,7 @@ public class AttributeDeclaration_Editor extends DefaultNodeEditor {
     {
       Style inlineStyle = new Style(editorCell) {
         {
-          this.set(StyleAttributes.PADDING_LEFT, new Padding(1.0, EnumMeasure.spaces));
+          this.set(StyleAttributes.PADDING_LEFT, new Padding(1.0, Measure.SPACES));
         }
 
       };
