5bda86aba12d76b4ce4d18bd7cb24045251a1580
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2009-02-20 17:21:25 +0300

    nontypesystem checker checks whether superconcepts of a concept are in a one of extended languages

--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/BeanCreator_Editor.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/BeanCreator_Editor.java
@@ -233,6 +233,7 @@ public class BeanCreator_Editor extends DefaultNodeEditor {
         EditorCell_Constant editorCell = new EditorCell_Constant(context, this.getOwner(), ",");
         editorCell.setSelectable(false);
         editorCell.getStyle().set(StyleAttributes.LAYOUT_CONSTRAINT, "");
+        editorCell.getStyle().set(StyleAttributes.PUNCTUATION_LEFT, true);
         return editorCell;
       }
     }
