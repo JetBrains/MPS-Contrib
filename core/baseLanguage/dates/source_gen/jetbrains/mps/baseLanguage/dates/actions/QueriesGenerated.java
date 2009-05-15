b9335410ef3b01fbdf53921929ab972dd0578e5d
Konstantin Solomatov <kostik@jetbrains.com>
2009-05-15 13:45:08 +0400

    fix unusual item in rt from dates lang

--- core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/actions/QueriesGenerated.java
+++ core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/actions/QueriesGenerated.java
@@ -7,7 +7,6 @@ import jetbrains.mps.smodel.action.SideTransformPreconditionContext;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
-import jetbrains.mps.smodel.BootstrapLanguages;
 import java.util.List;
 import jetbrains.mps.smodel.action.INodeSubstituteAction;
 import jetbrains.mps.smodel.action.SideTransformActionsBuilderContext;
@@ -45,10 +44,6 @@ public class QueriesGenerated {
       }
     }
     return dto != null && SLinkOperations.getTarget(dto, "datetimeProperty", false) == null;
-  }
-
-  public static boolean sideTransformHintSubstituteActionsBuilder_Precondition_Expression_1238258026282(final IOperationContext operationContext, final SideTransformPreconditionContext _context) {
-    return SNodeOperations.getParent(_context.getSourceNode()).getModel().hasLanguage(BootstrapLanguages.datesLanguage().getModuleReference());
   }
 
   public static boolean sideTransformHintSubstituteActionsBuilder_Precondition_Expression_1238418378523(final IOperationContext operationContext, final SideTransformPreconditionContext _context) {
@@ -169,31 +164,6 @@ public class QueriesGenerated {
 
         public String getDescriptionText(String pattern) {
           return "add compare precision";
-        }
-      });
-    }
-    return result;
-  }
-
-  public static List<INodeSubstituteAction> sideTransform_ActionsFactory_Expression_1238254013744(final IOperationContext operationContext, final SideTransformActionsBuilderContext _context) {
-    List<INodeSubstituteAction> result = ListSequence.fromList(new ArrayList<INodeSubstituteAction>());
-    {
-      SNode concept = SConceptOperations.findConceptDeclaration("jetbrains.mps.baseLanguage.dates.structure.PeriodConstant");
-      ListSequence.fromList(result).addElement(new AbstractSideTransformHintSubstituteAction(concept, _context.getSourceNode()) {
-
-        public SNode doSubstitute(String pattern) {
-          SNode period = SConceptOperations.createNewNode("jetbrains.mps.baseLanguage.dates.structure.PeriodConstant", null);
-          SLinkOperations.setTarget(period, "count", SNodeOperations.copyNode(_context.getSourceNode()), true);
-          SNodeOperations.replaceWithAnother(_context.getSourceNode(), period);
-          return period;
-        }
-
-        public String getMatchingText(String pattern) {
-          return " ";
-        }
-
-        public String getVisibleMatchingText(String pattern) {
-          return this.getMatchingText(pattern);
         }
       });
     }
