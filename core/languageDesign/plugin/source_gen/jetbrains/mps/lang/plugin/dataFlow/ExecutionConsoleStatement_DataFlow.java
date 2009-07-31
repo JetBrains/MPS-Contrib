7f6630756aee49fcf2d7a12111e1ed056b63e921
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-07-31 18:57:44 +0400

    dataflow for run configs

--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/dataFlow/ExecutionConsoleStatement_DataFlow.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/dataFlow/ExecutionConsoleStatement_DataFlow.java
@@ -5,8 +5,8 @@ package jetbrains.mps.lang.plugin.dataFlow;
 import jetbrains.mps.lang.dataFlow.DataFlowBuilder;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.lang.dataFlow.DataFlowBuilderContext;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 
 public class ExecutionConsoleStatement_DataFlow extends DataFlowBuilder {
 
@@ -14,7 +14,7 @@ public class ExecutionConsoleStatement_DataFlow extends DataFlowBuilder {
   }
 
   public void build(final IOperationContext operationContext, final DataFlowBuilderContext _context) {
-    _context.getBuilder().emitRead(SLinkOperations.getTarget(_context.getNode(), "consoleComponent", true));
+    _context.getBuilder().build((SNode)SLinkOperations.getTarget(_context.getNode(), "consoleComponent", true));
     _context.getBuilder().build((SNode)SLinkOperations.getTarget(_context.getNode(), "disposeBlock", true));
   }
 
