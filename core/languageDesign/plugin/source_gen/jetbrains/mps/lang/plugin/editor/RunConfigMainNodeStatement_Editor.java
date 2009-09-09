2d1323bb445f0c92e28d5559c34c27766b7ecfb3
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-09-09 19:49:50 +0400

    code minors

--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigMainNodeStatement_Editor.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigMainNodeStatement_Editor.java
@@ -21,14 +21,22 @@ public class RunConfigMainNodeStatement_Editor extends DefaultNodeEditor {
   private EditorCell createCollection_1339_0(EditorContext editorContext, SNode node) {
     EditorCell_Collection editorCell = EditorCell_Collection.createHorizontal(editorContext, node);
     editorCell.setCellId("Collection_1339_0");
-    editorCell.addEditorCell(this.createConstant_1339_0(editorContext, node));
+    editorCell.addEditorCell(this.createConstant_1339_1(editorContext, node));
     editorCell.addEditorCell(this.createRefNode_1339_0(editorContext, node));
+    editorCell.addEditorCell(this.createConstant_1339_0(editorContext, node));
     return editorCell;
   }
 
   private EditorCell createConstant_1339_0(EditorContext editorContext, SNode node) {
-    EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, node, "main node");
+    EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, node, ";");
     editorCell.setCellId("Constant_1339_0");
+    editorCell.setDefaultText("");
+    return editorCell;
+  }
+
+  private EditorCell createConstant_1339_1(EditorContext editorContext, SNode node) {
+    EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, node, "main node");
+    editorCell.setCellId("Constant_1339_1");
     editorCell.setDefaultText("");
     return editorCell;
   }
