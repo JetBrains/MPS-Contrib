ab7dcd6d75941baff9998fb31e0caa9fbe19d8eb
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-12-03 17:37:37 +0300

    eliminate orange code for run config creators

--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/dataFlow/CreateRunConfigStatement_DataFlow.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/dataFlow/CreateRunConfigStatement_DataFlow.java
@@ -5,8 +5,8 @@ package jetbrains.mps.lang.plugin.dataFlow;
 import jetbrains.mps.lang.dataFlow.DataFlowBuilder;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.lang.dataFlow.DataFlowBuilderContext;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 
 public class CreateRunConfigStatement_DataFlow extends DataFlowBuilder {
@@ -14,8 +14,8 @@ public class CreateRunConfigStatement_DataFlow extends DataFlowBuilder {
   }
 
   public void build(final IOperationContext operationContext, final DataFlowBuilderContext _context) {
-    _context.getBuilder().emitRead(SLinkOperations.getTarget(_context.getNode(), "configName", true));
-    _context.getBuilder().emitRead(SLinkOperations.getTarget(_context.getNode(), "suggestedName", true));
+    _context.getBuilder().build((SNode)SLinkOperations.getTarget(_context.getNode(), "configName", true));
+    _context.getBuilder().build((SNode)SLinkOperations.getTarget(_context.getNode(), "suggestedName", true));
     for (SNode property : ListSequence.fromList(SLinkOperations.getTargets(_context.getNode(), "property", true))) {
       _context.getBuilder().build((SNode)property);
     }
