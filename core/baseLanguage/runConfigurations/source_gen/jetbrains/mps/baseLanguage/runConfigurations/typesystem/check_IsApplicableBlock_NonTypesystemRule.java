233d3c25bec1a0448d542770b117cba5f16b465e
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2010-10-27 18:58:25 +0400

    BaseQuickFixProvider in typesystem generator - 10

--- core/baseLanguage/runConfigurations/source_gen/jetbrains/mps/baseLanguage/runConfigurations/typesystem/check_IsApplicableBlock_NonTypesystemRule.java
+++ core/baseLanguage/runConfigurations/source_gen/jetbrains/mps/baseLanguage/runConfigurations/typesystem/check_IsApplicableBlock_NonTypesystemRule.java
@@ -9,7 +9,7 @@ import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 import jetbrains.mps.lang.typesystem.runtime.IsApplicableStatus;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
-import jetbrains.mps.intentions.BaseIntentionProvider;
+import jetbrains.mps.errors.BaseQuickFixProvider;
 import jetbrains.mps.typesystem.inference.IErrorTarget;
 import jetbrains.mps.typesystem.inference.NodeErrorTarget;
 import jetbrains.mps.nodeEditor.IErrorReporter;
@@ -22,7 +22,7 @@ public class check_IsApplicableBlock_NonTypesystemRule extends AbstractNonTypesy
   public void applyRule(final SNode isApplicableBlock, final TypeCheckingContext typeCheckingContext, IsApplicableStatus status) {
     for (SNode t : ListSequence.fromList(SNodeOperations.getDescendants(isApplicableBlock, "jetbrains.mps.baseLanguage.structure.IThisExpression", false, new String[]{}))) {
       {
-        BaseIntentionProvider intentionProvider = null;
+        BaseQuickFixProvider intentionProvider = null;
         IErrorTarget errorTarget = new NodeErrorTarget();
         IErrorReporter _reporter_2309309498 = typeCheckingContext.reportTypeError(t, "Can't refer to \"this\" in \"is applicable\" block", "r:3d1d89d4-ed40-464f-804b-a59886f41d55(jetbrains.mps.baseLanguage.runConfigurations.typesystem)", "7976893777486251164", intentionProvider, errorTarget);
       }
