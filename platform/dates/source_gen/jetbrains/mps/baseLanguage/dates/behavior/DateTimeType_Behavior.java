768b57e45ac83bf848225371f7c5f9be828fe7d5
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2011-03-17 22:45:57 +0300

    regenerate models(fixing tests)

--- platform/dates/source_gen/jetbrains/mps/baseLanguage/dates/behavior/DateTimeType_Behavior.java
+++ platform/dates/source_gen/jetbrains/mps/baseLanguage/dates/behavior/DateTimeType_Behavior.java
@@ -30,7 +30,7 @@ public class DateTimeType_Behavior {
       {
         quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.ClassifierType", null, GlobalScope.getInstance(), false);
         SNode quotedNode1_2 = quotedNode_1;
-        quotedNode1_2.addReference(SReference.create("classifier", quotedNode1_2, SModelReference.fromString("f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(java.lang@java_stub)"), SNodeId.fromString("~Long")));
+        quotedNode1_2.addReference(SReference.create("classifier", quotedNode1_2, SModelReference.fromString("f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(JDK/java.lang@java_stub)"), SNodeId.fromString("~Long")));
         result = quotedNode1_2;
       }
       return result;
