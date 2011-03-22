30047a4d708c24e935d1722132d5a5ca11094bdd
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2011-03-22 17:02:34 +0300

    models regenerated - 6

--- platform/xmlUnitTest/languages/xmlUnitTest/source_gen/jetbrains/mps/xmlUnitTest/typesystem/typeof_XMLStringLiteral_InferenceRule.java
+++ platform/xmlUnitTest/languages/xmlUnitTest/source_gen/jetbrains/mps/xmlUnitTest/typesystem/typeof_XMLStringLiteral_InferenceRule.java
@@ -7,7 +7,6 @@ import jetbrains.mps.lang.typesystem.runtime.InferenceRule_Runtime;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 import jetbrains.mps.lang.typesystem.runtime.IsApplicableStatus;
-import jetbrains.mps.errors.BaseQuickFixProvider;
 import jetbrains.mps.typesystem.inference.EquationInfo;
 import jetbrains.mps.smodel.SModelUtil_new;
 import java.util.Set;
@@ -21,8 +20,7 @@ public class typeof_XMLStringLiteral_InferenceRule extends AbstractInferenceRule
   public void applyRule(final SNode literal, final TypeCheckingContext typeCheckingContext, IsApplicableStatus status) {
     {
       SNode _nodeToCheck_1029348928467 = literal;
-      BaseQuickFixProvider intentionProvider = null;
-      EquationInfo _info_12389875345 = new EquationInfo(_nodeToCheck_1029348928467, null, "r:d43ac4df-d36f-42ee-9923-a53d21b79525(jetbrains.mps.xmlUnitTest.typesystem)", "5211164146776491224", 0, intentionProvider);
+      EquationInfo _info_12389875345 = new EquationInfo(_nodeToCheck_1029348928467, null, "r:d43ac4df-d36f-42ee-9923-a53d21b79525(jetbrains.mps.xmlUnitTest.typesystem)", "5211164146776491224", 0, null);
       typeCheckingContext.createEquation((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:d43ac4df-d36f-42ee-9923-a53d21b79525(jetbrains.mps.xmlUnitTest.typesystem)", "5211164146776491221", true), (SNode) new typeof_XMLStringLiteral_InferenceRule.QuotationClass_e9zs51_a0a0a0().createNode(typeCheckingContext), _info_12389875345);
     }
   }
