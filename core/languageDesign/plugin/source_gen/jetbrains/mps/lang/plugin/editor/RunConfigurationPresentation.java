a6b019e8205d1032dfa81337b46098aba9b956df
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2011-04-13 19:09:05 +0400

    cleanup of EditorUtil class

--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigurationPresentation.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigurationPresentation.java
@@ -21,10 +21,11 @@ import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.nodeEditor.EditorManager;
 import jetbrains.mps.smodel.IScope;
 import jetbrains.mps.project.IModule;
-import jetbrains.mps.ide.EditorUtil;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.plugins.MacrosUtil;
 import jetbrains.mps.vfs.FileSystem;
 import javax.swing.JComponent;
+import jetbrains.mps.ide.EditorUtil;
 
 public class RunConfigurationPresentation extends AbstractCellProvider {
   public RunConfigurationPresentation(SNode node) {
@@ -220,7 +221,7 @@ public class RunConfigurationPresentation extends AbstractCellProvider {
 
   private static boolean renderingCondition_ltb2bm_a1b0(SNode node, EditorContext editorContext, IScope scope) {
     String path = null;
-    IModule module = EditorUtil.findAnchorModule(node);
+    IModule module = SNodeOperations.getModel(node).getModelDescriptor().getModule();
     if (module != null) {
       path = MacrosUtil.expandPath(SPropertyOperations.getString(node, "iconPath"), module.getModuleFqName());
     }
--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigurationTypeDeclaration_Editor.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/editor/RunConfigurationTypeDeclaration_Editor.java
@@ -24,10 +24,11 @@ import jetbrains.mps.nodeEditor.EditorManager;
 import jetbrains.mps.smodel.IScope;
 import jetbrains.mps.nodeEditor.BlockCells;
 import jetbrains.mps.project.IModule;
-import jetbrains.mps.ide.EditorUtil;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.plugins.MacrosUtil;
 import jetbrains.mps.vfs.FileSystem;
 import javax.swing.JComponent;
+import jetbrains.mps.ide.EditorUtil;
 
 public class RunConfigurationTypeDeclaration_Editor extends DefaultNodeEditor {
   public EditorCell createEditorCell(EditorContext editorContext, SNode node) {
@@ -320,7 +321,7 @@ public class RunConfigurationTypeDeclaration_Editor extends DefaultNodeEditor {
 
   private static boolean renderingCondition_wgm1kr_a1c1b0(SNode node, EditorContext editorContext, IScope scope) {
     String path = null;
-    IModule module = EditorUtil.findAnchorModule(node);
+    IModule module = SNodeOperations.getModel(node).getModelDescriptor().getModule();
     if (module != null) {
       path = MacrosUtil.expandPath(SPropertyOperations.getString(node, "iconPath"), module.getModuleFqName());
     }
