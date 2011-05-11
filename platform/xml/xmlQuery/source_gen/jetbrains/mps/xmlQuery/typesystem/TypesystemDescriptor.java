5c3f55605862189d66127a38a76d18eaa815eaab
Ilya Lintsbah <Ilya.Lintsbakh@jetbrains.com>
2011-05-11 14:29:10 +0400

    Revert "Regenerate"
    This reverts commit 0ebf70289f29d34b166d7c50cc2fad6e9a8e6a70.

--- platform/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/typesystem/TypesystemDescriptor.java
+++ platform/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/typesystem/TypesystemDescriptor.java
@@ -4,6 +4,7 @@ package jetbrains.mps.xmlQuery.typesystem;
 
 import jetbrains.mps.lang.typesystem.runtime.BaseHelginsDescriptor;
 import jetbrains.mps.lang.typesystem.runtime.InferenceRule_Runtime;
+import jetbrains.mps.lang.typesystem.runtime.NonTypesystemRule_Runtime;
 import jetbrains.mps.lang.typesystem.runtime.SubtypingRule_Runtime;
 
 public class TypesystemDescriptor extends BaseHelginsDescriptor {
@@ -63,6 +64,10 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
     {
       InferenceRule_Runtime inferenceRule = new typeof_XMLTextBuilder_InferenceRule();
       this.myInferenceRules.add(inferenceRule);
+    }
+    {
+      NonTypesystemRule_Runtime nonTypesystemRule = new check_XMLElementType_references_NonTypesystemRule();
+      this.myNonTypesystemRules.add(nonTypesystemRule);
     }
     {
       SubtypingRule_Runtime subtypingRule = new supertypeof_XMLElementType_SubtypingRule();
