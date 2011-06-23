b14ef4cd13515bf02ea1ec0159c576b07759a4ad
Timur Abishev <Timur.Abishev@jetbrains.com>
2011-06-23 19:49:33 +0400

    regenerate

--- core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/behavior/RunConfigurationDeclaration_BehaviorDescriptor.java
+++ core/languageDesign/plugin/source_gen/jetbrains/mps/lang/plugin/behavior/RunConfigurationDeclaration_BehaviorDescriptor.java
@@ -9,7 +9,7 @@ import jetbrains.mps.smodel.SNode;
 import java.util.List;
 import jetbrains.mps.baseLanguage.classifiers.behavior.IClassifier_Behavior;
 import jetbrains.mps.checkedName.PropertyReference;
-import jetbrains.mps.baseLanguage.behavior.AbstractExtractMethodRefactoringProcessor;
+import jetbrains.mps.baseLanguage.behavior.IExtractMethodRefactoringProcessor;
 import jetbrains.mps.lang.core.behavior.INamedConcept_Behavior;
 
 public class RunConfigurationDeclaration_BehaviorDescriptor extends BaseConcept_BehaviorDescriptor implements INamedConcept_BehaviorDescriptor, IClassifier_BehaviorDescriptor, ICheckedNamePolicy_BehaviorDescriptor {
@@ -44,7 +44,7 @@ public class RunConfigurationDeclaration_BehaviorDescriptor extends BaseConcept_
     return IClassifier_Behavior.virtual_createSuperType_1217433657148(thisNode);
   }
 
-  public AbstractExtractMethodRefactoringProcessor virtual_getExtractMethodRefactoringProcessor_1221393367929(SNode thisNode, List<SNode> nodesToRefactor) {
+  public IExtractMethodRefactoringProcessor virtual_getExtractMethodRefactoringProcessor_1221393367929(SNode thisNode, List<SNode> nodesToRefactor) {
     return IClassifier_Behavior.virtual_getExtractMethodRefactoringProcessor_1221393367929(thisNode, nodesToRefactor);
   }
 
