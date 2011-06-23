e85f291e489e374528c84561d222078ffda71973
Timur Abishev <Timur.Abishev@jetbrains.com>
2011-06-23 21:47:50 +0400

    regenerate 2

--- languages/util/buildlanguage/languages/generictasks/source_gen/jetbrains/mps/build/generictasks/behavior/BuiltInTaskDeclaration_BehaviorDescriptor.java
+++ languages/util/buildlanguage/languages/generictasks/source_gen/jetbrains/mps/build/generictasks/behavior/BuiltInTaskDeclaration_BehaviorDescriptor.java
@@ -7,7 +7,7 @@ import java.util.List;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.baseLanguage.classifiers.behavior.IClassifier_Behavior;
 import jetbrains.mps.buildlanguage.behavior.IDeclaration_Behavior;
-import jetbrains.mps.baseLanguage.behavior.AbstractExtractMethodRefactoringProcessor;
+import jetbrains.mps.baseLanguage.behavior.IExtractMethodRefactoringProcessor;
 import jetbrains.mps.lang.core.behavior.INamedConcept_Behavior;
 
 public class BuiltInTaskDeclaration_BehaviorDescriptor extends BaseConcept_BehaviorDescriptor implements ITaskDeclaration_BehaviorDescriptor {
@@ -58,7 +58,7 @@ public class BuiltInTaskDeclaration_BehaviorDescriptor extends BaseConcept_Behav
     return IClassifier_Behavior.virtual_createSuperType_1217433657148(thisNode);
   }
 
-  public AbstractExtractMethodRefactoringProcessor virtual_getExtractMethodRefactoringProcessor_1221393367929(SNode thisNode, List<SNode> nodesToRefactor) {
+  public IExtractMethodRefactoringProcessor virtual_getExtractMethodRefactoringProcessor_1221393367929(SNode thisNode, List<SNode> nodesToRefactor) {
     return IClassifier_Behavior.virtual_getExtractMethodRefactoringProcessor_1221393367929(thisNode, nodesToRefactor);
   }
 
