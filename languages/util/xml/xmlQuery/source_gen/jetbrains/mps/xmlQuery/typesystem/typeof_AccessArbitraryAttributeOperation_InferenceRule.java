052ff0bdb9d539f2289f4094855d40dad73627c4
Michael Vlassiev <Michael.Vlassiev@jetbrains.com>
2011-10-27 03:23:44 +0400

    make

--- languages/util/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/typesystem/typeof_AccessArbitraryAttributeOperation_InferenceRule.java
+++ languages/util/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/typesystem/typeof_AccessArbitraryAttributeOperation_InferenceRule.java
@@ -22,12 +22,12 @@ public class typeof_AccessArbitraryAttributeOperation_InferenceRule extends Abst
     {
       SNode _nodeToCheck_1029348928467 = operation;
       EquationInfo _info_12389875345 = new EquationInfo(_nodeToCheck_1029348928467, null, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "634907465121486825", 0, null);
-      typeCheckingContext.createEquation((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "634907465121486822", true), (SNode) new typeof_AccessArbitraryAttributeOperation_InferenceRule.QuotationClass_us6gyo_a0a0a0().createNode(), _info_12389875345);
+      typeCheckingContext.createEquation((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "634907465121486822", true), (SNode) new typeof_AccessArbitraryAttributeOperation_InferenceRule.QuotationClass_us6gyo_a0a0a0().createNode(typeCheckingContext), _info_12389875345);
     }
     {
       SNode _nodeToCheck_1029348928467 = SLinkOperations.getTarget(operation, "attributeNameExpression", true);
       EquationInfo _info_12389875345 = new EquationInfo(_nodeToCheck_1029348928467, null, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "4203809819610648514", 0, null);
-      typeCheckingContext.createLessThanInequality((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "4203809819610627154", true), (SNode) new typeof_AccessArbitraryAttributeOperation_InferenceRule.QuotationClass_us6gyo_a0a1a0().createNode(), false, true, _info_12389875345);
+      typeCheckingContext.createLessThanInequality((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:d89ddc4c-de44-408f-97b1-095c4af64bfb(jetbrains.mps.xmlQuery.typesystem)", "4203809819610627154", true), (SNode) new typeof_AccessArbitraryAttributeOperation_InferenceRule.QuotationClass_us6gyo_a0a1a0().createNode(typeCheckingContext), false, true, _info_12389875345);
     }
   }
 
@@ -50,6 +50,18 @@ public class typeof_AccessArbitraryAttributeOperation_InferenceRule extends Abst
     public QuotationClass_us6gyo_a0a0a0() {
     }
 
+    public SNode createNode(final TypeCheckingContext typeCheckingContext) {
+      SNode result = null;
+      Set<SNode> _parameterValues_129834374 = new HashSet<SNode>();
+      SNode quotedNode_1 = null;
+      {
+        quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.StringType", null, GlobalScope.getInstance(), false);
+        SNode quotedNode1_2 = quotedNode_1;
+        result = quotedNode1_2;
+      }
+      return result;
+    }
+
     public SNode createNode() {
       SNode result = null;
       Set<SNode> _parameterValues_129834374 = new HashSet<SNode>();
@@ -65,6 +77,18 @@ public class typeof_AccessArbitraryAttributeOperation_InferenceRule extends Abst
 
   public static class QuotationClass_us6gyo_a0a1a0 {
     public QuotationClass_us6gyo_a0a1a0() {
+    }
+
+    public SNode createNode(final TypeCheckingContext typeCheckingContext) {
+      SNode result = null;
+      Set<SNode> _parameterValues_129834374 = new HashSet<SNode>();
+      SNode quotedNode_1 = null;
+      {
+        quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.StringType", null, GlobalScope.getInstance(), false);
+        SNode quotedNode1_2 = quotedNode_1;
+        result = quotedNode1_2;
+      }
+      return result;
     }
 
     public SNode createNode() {
