ecb2bc2fdfffdf784a3989521033042431b6bbce
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-03-30 18:23:21 +0400

    rename editor tabs to relations move some relations classes from workbench to platform

--- languages/util/uiLanguage/solutions/pluginSolution/source_gen/jetbrains/mps/uiLanguage/pluginSolution/plugin/PluginSolution_ProjectPlugin.java
+++ languages/util/uiLanguage/solutions/pluginSolution/source_gen/jetbrains/mps/uiLanguage/pluginSolution/plugin/PluginSolution_ProjectPlugin.java
@@ -4,16 +4,17 @@ package jetbrains.mps.uiLanguage.pluginSolution.plugin;
 
 import jetbrains.mps.plugins.projectplugins.BaseProjectPlugin;
 import java.util.List;
-import jetbrains.mps.ide.editorTabs.EditorTabDescriptor;
+import jetbrains.mps.plugins.relations.RelationDescriptor;
 import com.intellij.openapi.project.Project;
+
 import java.util.ArrayList;
 
 public class PluginSolution_ProjectPlugin extends BaseProjectPlugin {
   public PluginSolution_ProjectPlugin() {
   }
 
-  public List<EditorTabDescriptor> initTabbedEditors(Project project) {
-    List<EditorTabDescriptor> result = new ArrayList();
+  public List<RelationDescriptor> initTabbedEditors(Project project) {
+    List<RelationDescriptor> result = new ArrayList();
     result.add(new Component_TabDescriptor());
     result.add(new Controller_TabDescriptor());
     return result;
