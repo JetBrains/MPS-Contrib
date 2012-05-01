828acab3a1304707aa80d0981bd0d3264edad5be
Timur Abishev <timur.abishev@jetbrains.com>
2012-05-01 18:17:00 +0400

    regenerate some behaviors

--- languages/util/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/behavior/XMLElementType_Behavior.java
+++ languages/util/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/behavior/XMLElementType_Behavior.java
@@ -6,8 +6,8 @@ import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
-import jetbrains.mps.smodel.structure.BehaviorDescriptor;
-import jetbrains.mps.smodel.structure.ConceptRegistry;
+import jetbrains.mps.smodel.runtime.BehaviorDescriptor;
+import jetbrains.mps.smodel.language.ConceptRegistry;
 import jetbrains.mps.smodel.behaviour.BehaviorManager;
 
 public class XMLElementType_Behavior {
