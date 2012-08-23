edfa4f9a6e514f7c22c6515c66d49832bc0859aa
Ilya Lintsbakh <ilya.lintsbakh@jetbrains.com>
2012-08-23 15:39:55 +0400

    Regenerate

--- languages/util/dates/source_gen/jetbrains/mps/baseLanguage/dates/behavior/PeriodPropertyFormatToken_Behavior.java
+++ languages/util/dates/source_gen/jetbrains/mps/baseLanguage/dates/behavior/PeriodPropertyFormatToken_Behavior.java
@@ -7,6 +7,6 @@ import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
 
 public class PeriodPropertyFormatToken_Behavior {
   public static void init(SNode thisNode) {
-    SPropertyOperations.set(thisNode, "minDigits", "" + 1);
+    SPropertyOperations.set(thisNode, "minDigits", "" + (1));
   }
 }
