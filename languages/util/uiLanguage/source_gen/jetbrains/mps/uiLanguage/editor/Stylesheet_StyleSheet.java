c9519199017a36fb72b501ea721fc04b62d5fbb4
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2013-01-21 22:28:58 +0400

    MPS-15260 editor is not updated automatically if query in a style changes its value - regenerating all editors

--- languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/Stylesheet_StyleSheet.java
+++ languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/Stylesheet_StyleSheet.java
@@ -9,43 +9,43 @@ import jetbrains.mps.nodeEditor.MPSColors;
 import jetbrains.mps.nodeEditor.MPSFonts;
 
 public class Stylesheet_StyleSheet {
-  public static Style getAttribute(final EditorCell cell) {
-    Style style = new Style(cell);
+  public static Style getAttribute(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
     style.set(StyleAttributes.TEXT_COLOR, MPSColors.DARK_MAGENTA);
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.PLAIN);
     return style;
   }
 
-  public static Style getComponent(final EditorCell cell) {
-    Style style = new Style(cell);
+  public static Style getComponent(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
     style.set(StyleAttributes.TEXT_COLOR, MPSColors.blue);
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.BOLD);
     return style;
   }
 
-  public static Style getComponentInstanceName(final EditorCell cell) {
-    Style style = new Style(cell);
+  public static Style getComponentInstanceName(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
     style.set(StyleAttributes.TEXT_COLOR, MPSColors.blue);
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.ITALIC);
     return style;
   }
 
-  public static Style getLayoutConstraint(final EditorCell cell) {
-    Style style = new Style(cell);
-    style.putAll(Stylesheet_StyleSheet.getAttribute(null));
+  public static Style getLayoutConstraint(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
+    style.putAll(Stylesheet_StyleSheet.getAttribute(editorCell));
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.BOLD);
     return style;
   }
 
-  public static Style getActionHandler(final EditorCell cell) {
-    Style style = new Style(cell);
+  public static Style getActionHandler(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
     style.set(StyleAttributes.TEXT_COLOR, MPSColors.DARK_GREEN);
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.PLAIN);
     return style;
   }
 
-  public static Style getModel(final EditorCell cell) {
-    Style style = new Style(cell);
+  public static Style getModel(final EditorCell editorCell) {
+    Style style = new Style(editorCell);
     style.set(StyleAttributes.TEXT_COLOR, MPSColors.DARK_BLUE);
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.PLAIN);
     return style;
