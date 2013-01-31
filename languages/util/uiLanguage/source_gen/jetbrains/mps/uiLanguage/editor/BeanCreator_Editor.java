9797b1816b3597ea47dedc6ac632d48f0f9b8a9f
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2013-01-31 16:12:43 +0400

    Regenerating all editors

--- languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/BeanCreator_Editor.java
+++ languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/editor/BeanCreator_Editor.java
@@ -102,7 +102,7 @@ public class BeanCreator_Editor extends DefaultNodeEditor {
     }
 
     @Override
-    public EditorCell createSeparatorCell(EditorContext editorContext, SNode node) {
+    public EditorCell createSeparatorCell(EditorContext editorContext, SNode prevNode, SNode nextNode) {
       EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, this.getOwner(), ",");
       editorCell.setSelectable(false);
       Style style = new StyleImpl();
