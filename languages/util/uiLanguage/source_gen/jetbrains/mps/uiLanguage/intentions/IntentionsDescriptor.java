c5bee0c4e35c26eb81e4988fe6d10b84a8cd663e
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2013-03-13 14:23:15 +0400

    use openapi SNodeId/ModelReference constructors in intentions

--- languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/intentions/IntentionsDescriptor.java
+++ languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/intentions/IntentionsDescriptor.java
@@ -4,12 +4,12 @@ package jetbrains.mps.uiLanguage.intentions;
 
 import jetbrains.mps.intentions.BaseIntentionsDescriptor;
 import jetbrains.mps.project.structure.modules.ModuleReference;
-import jetbrains.mps.smodel.SModelReference;
+import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
 import jetbrains.mps.intentions.IntentionsManager;
 
 public class IntentionsDescriptor extends BaseIntentionsDescriptor {
   public IntentionsDescriptor() {
-    super(new ModuleReference("5d6bee4c-f891-4a93-a0c9-e2268726ae47(jetbrains.mps.uiLanguage)"), SModelReference.fromString("r:00000000-0000-4000-0000-011c89590551(jetbrains.mps.uiLanguage.intentions)"));
+    super(new ModuleReference("5d6bee4c-f891-4a93-a0c9-e2268726ae47(jetbrains.mps.uiLanguage)"), PersistenceFacade.getInstance().createModelReference("r:00000000-0000-4000-0000-011c89590551(jetbrains.mps.uiLanguage.intentions)"));
   }
 
   public void init() {
