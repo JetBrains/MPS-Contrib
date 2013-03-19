9180535bfa8a077e88b2093a1e3290994901e07d
Timur Abishev <timur.abishev@jetbrains.com>
2013-03-20 02:35:22 +0400

    Fix diff tests

--- languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/Language.java
+++ languages/util/uiLanguage/source_gen/jetbrains/mps/uiLanguage/Language.java
@@ -4,6 +4,7 @@ package jetbrains.mps.uiLanguage;
 
 import jetbrains.mps.smodel.language.LanguageRuntime;
 import org.jetbrains.mps.openapi.module.SModuleReference;
+import jetbrains.mps.project.structure.modules.ModuleReference;
 import jetbrains.mps.lang.typesystem.runtime.IHelginsDescriptor;
 import jetbrains.mps.uiLanguage.typesystem.TypesystemDescriptor;
 import jetbrains.mps.ide.findusages.BaseFindUsagesDescriptor;
@@ -12,7 +13,7 @@ import jetbrains.mps.generator.runtime.TemplateModule;
 import jetbrains.mps.generator.runtime.TemplateUtil;
 
 public class Language extends LanguageRuntime {
-  public static SModuleReference MODULE_REFERENCE = jetbrains.mps.project.structure.modules.ModuleReference.fromString("5d6bee4c-f891-4a93-a0c9-e2268726ae47(jetbrains.mps.uiLanguage)");
+  public static SModuleReference MODULE_REFERENCE = ModuleReference.fromString("5d6bee4c-f891-4a93-a0c9-e2268726ae47(jetbrains.mps.uiLanguage)");
 
   public Language() {
 
