2b06d9fc291f8568873cca590c1996b7b85c9ed3
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2013-09-20 17:48:28 +0200

    rebuildingeditors in accordance with latest changes from 3.0

--- mps-dates/languages/dates/source_gen/jetbrains/mps/baseLanguage/dates/editor/DateTimeOffsetFormatToken_Editor.java
+++ mps-dates/languages/dates/source_gen/jetbrains/mps/baseLanguage/dates/editor/DateTimeOffsetFormatToken_Editor.java
@@ -114,6 +114,8 @@ public class DateTimeOffsetFormatToken_Editor extends DefaultNodeEditor {
       style.set(StyleAttributes.LAYOUT_CONSTRAINT, "");
       style.set(StyleAttributes.PUNCTUATION_LEFT, true);
       editorCell.getStyle().putAll(style);
+      editorCell.setAction(CellActionType.BACKSPACE, new CellAction_DeleteNode(prevNode));
+      editorCell.setAction(CellActionType.DELETE, new CellAction_DeleteNode(prevNode));
       return editorCell;
     }
 
