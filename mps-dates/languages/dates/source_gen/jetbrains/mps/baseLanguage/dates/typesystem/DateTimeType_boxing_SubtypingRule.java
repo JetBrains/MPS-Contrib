d61befa5164b8fb8119196c564e29d747fe7da6f
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2013-12-19 12:15:39 +0100

    Regenerated to pick up recent changes in mps.build.mps (generate tast parameters) and lang.pattern (human-friendly names)

--- mps-dates/languages/dates/source_gen/jetbrains/mps/baseLanguage/dates/typesystem/DateTimeType_boxing_SubtypingRule.java
+++ mps-dates/languages/dates/source_gen/jetbrains/mps/baseLanguage/dates/typesystem/DateTimeType_boxing_SubtypingRule.java
@@ -49,14 +49,13 @@ public class DateTimeType_boxing_SubtypingRule extends SubtypingRule_Runtime imp
 
     public boolean match(SNode nodeToMatch) {
       {
-        SNode nodeToMatch_DateTimeType_boxing_u5vsb4_a0a;
-        nodeToMatch_DateTimeType_boxing_u5vsb4_a0a = nodeToMatch;
-        if (!("jetbrains.mps.baseLanguage.structure.ClassifierType".equals(nodeToMatch_DateTimeType_boxing_u5vsb4_a0a.getConcept().getQualifiedName()))) {
+        SNode nodeToMatch_u5vsb4_a0a = nodeToMatch;
+        if (!("jetbrains.mps.baseLanguage.structure.ClassifierType".equals(nodeToMatch_u5vsb4_a0a.getConcept().getQualifiedName()))) {
           return false;
         }
         {
-          SNodeReference pointer = SNODE_POINTER_enqoju_a0a0a0a0b0c0a0a0a0a0a0e;
-          if (!(PatternUtil.matchReferentWithNode(pointer, nodeToMatch_DateTimeType_boxing_u5vsb4_a0a.getReferenceTarget("classifier")))) {
+          SNodeReference pointer = SNODE_POINTER_enqoju_a0a0a0a0b0b0a0a0a0a0a0e;
+          if (!(PatternUtil.matchReferentWithNode(pointer, nodeToMatch_u5vsb4_a0a.getReferenceTarget("classifier")))) {
             return false;
           }
         }
@@ -86,5 +85,5 @@ public class DateTimeType_boxing_SubtypingRule extends SubtypingRule_Runtime imp
     return quotedNode_1;
   }
 
-  private static SNodePointer SNODE_POINTER_enqoju_a0a0a0a0b0c0a0a0a0a0a0e = new SNodePointer("f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(JDK/java.lang@java_stub)", "~Long");
+  private static SNodePointer SNODE_POINTER_enqoju_a0a0a0a0b0b0a0a0a0a0a0e = new SNodePointer("f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(JDK/java.lang@java_stub)", "~Long");
 }
